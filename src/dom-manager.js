da36bece12fe5bca397ded478516a75a35456bc1
wesley <wesley_snaine@hotmail.com>
2023-02-19 17:44:53 -0300

    dom manager can add additional information from weather on the dom

--- src/dom-manager.js
+++ src/dom-manager.js
@@ -10,10 +10,12 @@ class DOMManager {
     #clouds;
     #maxMin;
     #pressure;
-    #rain;
     #humidity;
     #air;
+    #rain;
+    #rainContainer;
     #snow;
+    #snowContainer;
     constructor() {
         this.#location = document.querySelector('.location');
         this.#icon = document.querySelector('.icon');
@@ -29,7 +31,10 @@ class DOMManager {
         this.#rain = document.querySelector('.rain');
         this.#humidity = document.querySelector('.humidity');
         this.#air = document.querySelector('.air-quality');
+        this.#rain = document.querySelector('.rain');
+        this.#rainContainer = document.querySelector('#rain-container');
         this.#snow = document.querySelector('.snow');
+        this.#snowContainer = document.querySelector('#snow-container');
     }
 
     updateWeatherData(data) {
@@ -43,10 +48,10 @@ class DOMManager {
         const time = new Date(data.dt * 1000);
         const temp = data.main.temp;
         const tempColors = ['#ff3300', '#ff6600', '#ff9933', '#ffcc99', '#f2f2f2', '#cce6ff', '#80ciff', '#0084ff', '#4d4dff', '#7733ff']
-        this.#location.textContent = data.name + ', ' + (data.state || data.sys.country);
+        this.#location.textContent = `${data.name}, ${(data.state || data.sys.country)}`;
         this.#icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
         this.#icon.alt = data.weather[0].description;
-        this.#temperature.textContent = Math.round(temp) + 'ºC';
+        this.#temperature.textContent = `${Math.round(temp)}ºC`;
         //TODO: a better way to do it, this is ugly as fuck
         if (temp >= 35) {
             this.#temperature.style.color = tempColors[0];
@@ -70,17 +75,35 @@ class DOMManager {
             this.#temperature.style.color = tempColors[9];
         }
         this.#description.textContent = data.weather[0].description;
-        this.#time.textContent = time.getHours() + ':' + time.getMinutes();
+        this.#time.textContent = `${time.getHours()}:${time.getMinutes()}`;
     }
 
     #updateAdditionalWeather(data) {
-        this.#feelsTemp.textContent = Math.round(data.main.feels_like) + 'ºC';
+        this.#feelsTemp.textContent = `${Math.round(data.main.feels_like)}ºC`;
         /* Wind direction: https://en.wikipedia.org/wiki/Wind_direction
            0deg = north 90deg = east    180deg = south  270deg = west
            arrow-up     arrow-right     arrow-down      arrow-left
         */
-       this.#windDirection.style.transform = `rotate(${-(data.wind.deg)}deg)`;
-       this.#windSpeed.textContent = data.wind.speed + 'mph';
+        this.#windDirection.style.transform = `rotate(${-(data.wind.deg)}deg)`;
+        this.#windSpeed.textContent = `${data.wind.speed}mph`;
+        this.#clouds.textContent = `${data.clouds.all}%`;
+        this.#maxMin.textContent = `${data.main.temp_max}º/${data.main.temp_min}º`;
+        this.#pressure.textContent = `${data.main.pressure}hPa`;
+        this.#humidity.textContent = `${data.main.humidity}%`;
+        console.log(data.rain);
+        console.log(data.snow);
+        if (data.rain) {
+            this.#rain.textContent = `${data.rain['1h']}mm`;
+            this.#rainContainer.style.display = 'flex';
+        } else {
+            this.#rainContainer.style.display = 'none';
+        }
+        if (data.snow) {
+            this.#snow.textContent = `${data.snow['1h']}mm`;
+            this.#snowContainer.style.display = 'flex';
+        } else {
+            this.#snowContainer.style.display = 'none';
+        }
     }
 
     #updateBackgroundImage(weather, temp) {
@@ -117,8 +140,6 @@ class DOMManager {
         document.querySelector('main').style.backgroundImage = image;
     }
 }
-
-
 
 const manager = new DOMManager();
 
