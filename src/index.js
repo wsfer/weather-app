85c2321dc9df28aabc2e7bef945e40a8ae05df0a
wesley <wesley_snaine@hotmail.com>
2023-02-23 20:26:19 -0300

    add search function

--- src/index.js
+++ src/index.js
@@ -4,20 +4,35 @@ import DOMManager from './dom-manager.js';
 import ForecastCarousel from './forecast-carousel.js';
 
 let forecastCarousel;
+const loadingScreen = document.querySelector('#loading');
+const input = document.querySelector('.input-container > input');
+
+document.querySelector('#search-box > button').addEventListener('click', () => {
+    searchForACity(input.value)
+        .then((coords) => {
+            updateWeather(...coords);
+        });
+});
 
 //Try getting user location
 navigator.geolocation.getCurrentPosition((location) => {
     const lat = location.coords.latitude;
     const lon = location.coords.longitude;
-    WeatherAPI.getCurrentWeather(lat, lon)
-        .then((data) => DOMManager.updateWeatherData(data));
-    
-    setTimeout(() => {
-        WeatherAPI.getForecast(lat, lon)
-            .then((data) => DOMManager.updateForecastData(data))
-            .then((carousel) => forecastCarousel = new ForecastCarousel(...carousel))
-            .then(() => {
-                document.querySelector('#loading').style.height = '0';
-            });
-    }, 1200);
+    updateWeather(lat, lon);
 });
+
+async function searchForACity(city) {
+    const cityLocation = await WeatherAPI.getCityLocation(city);
+    return [cityLocation[0].lat, cityLocation[0].lon];
+}
+
+async function updateWeather(lat, lon) {
+    loadingScreen.style.height = '100vh';
+    const currentWeather = await WeatherAPI.getCurrentWeather(lat, lon);
+    const currentForecast = await WeatherAPI.getForecast(lat, lon);
+    const airQuality = await WeatherAPI.getAirData(lat, lon);
+    DOMManager.updateWeatherData(currentWeather, airQuality);
+    const carousel = DOMManager.updateForecastData(currentForecast); //this function return the elements needed for ForecastCarousel creation
+    forecastCarousel = new ForecastCarousel(...carousel);
+    loadingScreen.style.height = '0';
+}
