e625895278cd7d54c7654f657aa3e3695ade7f28
wsfer <93054415+wsfer@users.noreply.github.com>
2023-11-23 17:26:14 -0300

    build: update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather',
+      favicon: './src/assets/favicon.svg',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
