119f1a04b367450e3f8af52e0a1ec84a532f964d
wsfer <93054415+wsfer@users.noreply.github.com>
2023-11-23 14:38:42 -0300

    feat: create function to get country flag emoji

---
+++ src/assets/getFlagEmoji.js
@@ -0,0 +1,9 @@
+const getFlagEmoji = (countryCode) => {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+};
+
+export default getFlagEmoji;
