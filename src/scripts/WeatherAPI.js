04f8fce948c1ea7db9f8685db83575ef1227173a
wsfer <93054415+wsfer@users.noreply.github.com>
2023-11-23 14:38:18 -0300

    feat: recreate weather api functions

---
+++ src/scripts/WeatherAPI.js
@@ -0,0 +1,33 @@
+const API_KEY = '53adf48f9adafdb91a2ae308a53f4cbd';
+
+const getCityLocations = async (cityName) => {
+  const endpoint = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return response.ok
+    ? { cities: data, error: '' }
+    : { cities: [], error: data.message };
+};
+
+const getCurrentWeather = async (lat, lon) => {
+  const endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data;
+};
+
+const getWeatherForecast = async (lat, lon) => {
+  const endpoint = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data;
+};
+
+const getAirData = async (lat, lon) => {
+  const endpoint = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data;
+};
+
+export { getCityLocations, getCurrentWeather, getWeatherForecast, getAirData };
