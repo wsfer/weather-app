baec993335571ba100cf9fcecbb41ae327a129d5
wsfer <93054415+wsfer@users.noreply.github.com>
2023-11-23 18:08:09 -0300

    feat: create function to dynamically update page content

--- src/scripts/updateWeather.js
+++ src/scripts/updateWeather.js
@@ -1,6 +1,18 @@
+import { getCurrentWeather, getAirData } from './WeatherAPI';
+import weatherMain from '../components/weatherMain';
+import weatherStatus from '../components/weatherStatus';
+
 // Recreate HTML sections with new weather informations
-const updateWeather = (lat, lon) => {
-  console.log({ lat, lon });
+const updateWeather = async (lat, lon) => {
+  const dynamicDOMContent = document.querySelector('.js-content');
+  const currentWeatherData = await getCurrentWeather(lat, lon);
+  const airData = await getAirData(lat, lon);
+
+  const DOMWeatherMain = weatherMain(currentWeatherData);
+  const DOMWeatherStatus = weatherStatus(currentWeatherData, airData);
+
+  dynamicDOMContent.appendChild(DOMWeatherMain);
+  dynamicDOMContent.appendChild(DOMWeatherStatus);
 };
 
 export default updateWeather;
