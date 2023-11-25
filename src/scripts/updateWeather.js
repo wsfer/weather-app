1e1b8a92a377f9e918198c0105a1c3df8adec11e
wsfer <93054415+wsfer@users.noreply.github.com>
2023-11-24 23:59:32 -0300

    feat: include background component on update weather function

--- src/scripts/updateWeather.js
+++ src/scripts/updateWeather.js
@@ -1,18 +1,24 @@
 import { getCurrentWeather, getAirData } from './WeatherAPI';
 import weatherMain from '../components/weatherMain';
 import weatherStatus from '../components/weatherStatus';
+import getWeatherImage from '../assets/getWeatherImage';
 
 // Recreate HTML sections with new weather informations
 const updateWeather = async (lat, lon) => {
   const dynamicDOMContent = document.querySelector('.js-content');
+  const backgroundImageContainer = document.querySelector('.js-background');
+
   const currentWeatherData = await getCurrentWeather(lat, lon);
   const airData = await getAirData(lat, lon);
 
   const DOMWeatherMain = weatherMain(currentWeatherData);
   const DOMWeatherStatus = weatherStatus(currentWeatherData, airData);
+  const weather = currentWeatherData.weather[0].main;
+  const image = getWeatherImage(weather);
 
   dynamicDOMContent.appendChild(DOMWeatherMain);
   dynamicDOMContent.appendChild(DOMWeatherStatus);
+  backgroundImageContainer.style.backgroundImage = `linear-gradient(rgb(0, 0, 0, 0.7), rgb(0, 0, 0, 0.7)), url('${image}')`;
 };
 
 export default updateWeather;
