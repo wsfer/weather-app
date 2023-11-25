eaf528abd945b97c7c818195de82854afe2d4a3e
wsfer <93054415+wsfer@users.noreply.github.com>
2023-11-25 17:07:14 -0300

    feat: include weather forecast component on update weather function

--- src/scripts/updateWeather.js
+++ src/scripts/updateWeather.js
@@ -1,6 +1,11 @@
-import { getCurrentWeather, getAirData } from './WeatherAPI';
+import {
+  getCurrentWeather,
+  getWeatherForecast,
+  getAirData,
+} from './WeatherAPI';
 import weatherMain from '../components/weatherMain';
 import weatherStatus from '../components/weatherStatus';
+import weatherForecast from '../components/weatherForecast';
 import getWeatherImage from '../assets/getWeatherImage';
 
 // Recreate HTML sections with new weather informations
@@ -9,15 +14,18 @@ const updateWeather = async (lat, lon) => {
   const backgroundImageContainer = document.querySelector('.js-background');
 
   const currentWeatherData = await getCurrentWeather(lat, lon);
+  const forecastData = await getWeatherForecast(lat, lon);
   const airData = await getAirData(lat, lon);
 
   const DOMWeatherMain = weatherMain(currentWeatherData);
   const DOMWeatherStatus = weatherStatus(currentWeatherData, airData);
+  const DOMWeatherForecast = weatherForecast(forecastData);
   const weather = currentWeatherData.weather[0].main;
   const image = getWeatherImage(weather);
 
   dynamicDOMContent.appendChild(DOMWeatherMain);
   dynamicDOMContent.appendChild(DOMWeatherStatus);
+  dynamicDOMContent.appendChild(DOMWeatherForecast);
   backgroundImageContainer.style.backgroundImage = `linear-gradient(rgb(0, 0, 0, 0.7), rgb(0, 0, 0, 0.7)), url('${image}')`;
 };
 
