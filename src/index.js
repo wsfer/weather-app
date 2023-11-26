c5606a8bb6037c69478c045b0967ad03242df477
wsfer <93054415+wsfer@users.noreply.github.com>
2023-11-25 22:38:39 -0300

    feat: include loading screen on page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import './styles/main.scss';
 import header from './components/header';
 import footer from './components/footer';
+import loading from './components/loading';
 import updateWeather from './scripts/updateWeather';
 
 const defaultCoords = [0, 0]; // TODO: add more coords
-const dynamicDOMContent = new Range().createContextualFragment(
-  '<div class="js-content content"></div>'
-);
+const dynamicDOMContent = new Range()
+  .createContextualFragment('<div class="js-content content"></div>')
+  .querySelector('div');
 const backgroundImageContainer = new Range()
   .createContextualFragment('<div class="js-background background"></div>')
   .querySelector('div');
 
+dynamicDOMContent.appendChild(loading());
 backgroundImageContainer.append(header);
 backgroundImageContainer.append(dynamicDOMContent);
 backgroundImageContainer.append(footer);
--- src/scripts/updateWeather.js
+++ src/scripts/updateWeather.js
@@ -7,11 +7,15 @@ import weatherMain from '../components/weatherMain';
 import weatherStatus from '../components/weatherStatus';
 import weatherForecast from '../components/weatherForecast';
 import getWeatherImage from '../assets/getWeatherImage';
+import loading from '../components/loading';
 
 // Recreate HTML sections with new weather informations
 const updateWeather = async (lat, lon) => {
   const dynamicDOMContent = document.querySelector('.js-content');
   const backgroundImageContainer = document.querySelector('.js-background');
+
+  dynamicDOMContent.textContent = ''; // Clear old content
+  dynamicDOMContent.appendChild(loading());
 
   const currentWeatherData = await getCurrentWeather(lat, lon);
   const forecastData = await getWeatherForecast(lat, lon);
@@ -23,6 +27,7 @@ const updateWeather = async (lat, lon) => {
   const weather = currentWeatherData.weather[0].main;
   const image = getWeatherImage(weather);
 
+  dynamicDOMContent.textContent = ''; // Remove loading bar
   dynamicDOMContent.appendChild(DOMWeatherMain);
   dynamicDOMContent.appendChild(DOMWeatherStatus);
   dynamicDOMContent.appendChild(DOMWeatherForecast);
