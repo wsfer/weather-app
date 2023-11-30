4191c41690e083882f770e766d3567a424a2a5e2
wsfer <93054415+wsfer@users.noreply.github.com>
2023-11-29 22:09:21 -0300

    feat: include weather map on update weather function

--- src/scripts/updateWeather.js
+++ src/scripts/updateWeather.js
@@ -6,6 +6,7 @@ import {
 import weatherMain from '../components/weatherMain';
 import weatherStatus from '../components/weatherStatus';
 import weatherForecast from '../components/weatherForecast';
+import weatherMap from '../components/weatherMap';
 import getWeatherImage from './getWeatherImage';
 import loading from '../components/loading';
 
@@ -24,6 +25,7 @@ const updateWeather = async (lat, lon) => {
   const DOMWeatherMain = weatherMain(currentWeatherData);
   const DOMWeatherStatus = weatherStatus(currentWeatherData, airData);
   const DOMWeatherForecast = weatherForecast(forecastData);
+  const DOMWeatherMap = weatherMap(lat, lon);
   const weather = currentWeatherData.weather[0].main;
   const temperature = currentWeatherData.main.temp;
   const image = getWeatherImage(weather, temperature);
@@ -32,6 +34,7 @@ const updateWeather = async (lat, lon) => {
   dynamicDOMContent.appendChild(DOMWeatherMain);
   dynamicDOMContent.appendChild(DOMWeatherStatus);
   dynamicDOMContent.appendChild(DOMWeatherForecast);
+  dynamicDOMContent.appendChild(DOMWeatherMap);
   backgroundImageContainer.style.backgroundImage = `linear-gradient(rgb(0, 0, 0, 0.7), rgb(0, 0, 0, 0.7)), url('${image}')`;
 };
 
